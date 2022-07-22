a8bfea2ba3c57b937dd19bd4b937ffdbddf322de
Eddahkarambu <gikundaeddah@gmail.com>
2022-07-22 20:01:07 +0300

    add style in cartitem

--- src/Components/CartOverlay.js
+++ src/Components/CartOverlay.js
@@ -1,5 +1,5 @@
 import { Fragment,Component } from "react";
-import {Overlay,} from "./CartOverlay.style"
+import {Overlay,CheckoutButtons,Costs,Button1,Button2} from "./CartOverlay.style"
 import CartItem from './CartItem';
 
 
@@ -12,12 +12,15 @@ class CartOverlay extends Component  {
               <Overlay>
                 <CartItem />
                 <CartItem />
-                <div>
-
-                </div>
-                <div>
-                  
-                </div>
+                <Costs>
+                  <div>Total</div>
+                  <div>$ 200.00</div>
+                </Costs>
+             
+                <CheckoutButtons>
+                  <Button1>VIEW BAG</Button1>
+                <Button2>CHECK OUT</Button2>
+                </CheckoutButtons>
    
               </Overlay>
             )}
--- src/Components/CartOverlay.style.js
+++ src/Components/CartOverlay.style.js
@@ -11,6 +11,39 @@ padding: 32px 16px;
 width: 325px;
 // height:500px;
 `
+export const Costs = styled.div`
+display:flex;
+justify-content:space-between;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+
+`
+
+export const CheckoutButtons = styled.div`
+display:flex;
+justify-content:space-between;
+margin-top:1rem;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+
+`
+export const Button1 = styled.button`
+background-color:white;
+Padding:16px 32px;
+border: 1px solid #1D1F22;
+
+`
+export const Button2 = styled.button`
+background-color:green;
+Padding:16px 32px;
+color:white;
+border: 1px solid #1D1F22;
+`
+
 
 
 
