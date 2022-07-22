9a309e00cca7983ecaaab1860556b680ce934467
Eddahkarambu <gikundaeddah@gmail.com>
2022-07-22 14:06:04 +0300

    craete divs in cart item to represent each item located in the cartitem.js folder

---
+++ src/Components/ProductImage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Image} from "./ProductColor.style"
+
+class ProductImage extends Component  {
+   
+    render(){
+        return(
+            <Image>
+                <div></div>
+            </Image>
+        )
+    }
+}
+export default ProductImage;
