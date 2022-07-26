33bbfc01071b02f23e9faefc2bfad6d25aaae90d
Eddahkarambu <gikundaeddah@gmail.com>
2022-07-26 17:11:23 +0300

    add product.js  path on the router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
-import Cartpage from "./Cartpage";
+import CartPage from "./CartPage";
 import Category from "./Category";
 import Product from "./Product";
 
@@ -11,7 +11,8 @@ export default class Paths extends Component {
             <Routes>
                     <Route path="/" exact element={<Category/>}></Route>
                     <Route path="/Product" exact element={<Product/>}></Route>
-                    <Route path="/Cartpage" exact element={<Cartpage/>}></Route>
+                    <Route path="/CartPage" exact element={<CartPage/>}></Route>
+                    <Route path="/Product" exact element={<Product/>}></Route>
                 </Routes>
         )
     }
