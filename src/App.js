52e85667eb6e6578d8f6c8b2bd9d42aac7e3b5c9
Eddahkarambu <gikundaeddah@gmail.com>
2022-07-27 12:55:12 +0300

    import grapghql, apollo-boost and react-apollo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Paths from "./Components/Router";
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 import { BrowserRouter as Router } from "react-router-dom";
+
+
+const client =new ApolloClient({
+  uri:"http://localhost:4000/"
+});
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client ={client}>
       <Router>
       <Paths />
     </Router>
+    </ApolloProvider>
     );
   }
 }
---
+++ src/Components/Data.js
@@ -0,0 +1,3 @@
+import react from 'react';
+import {Query} from 'react-apollo';
+import {gql} from 'graphql-tag'
