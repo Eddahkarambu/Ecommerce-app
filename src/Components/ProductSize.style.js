e994bbc99cdb55eb16095aa18c63b4dff11f39d1
Eddahkarambu <gikundaeddah@gmail.com>
2022-08-16 21:27:16 +0300

    delete node-modules and installed it again

--- src/Components/ProductSize.style.js
+++ src/Components/ProductSize.style.js
@@ -1,17 +1,16 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Box = styled.div`
-
-border: 1px solid black;
-margin: 0 0.25rem;
-padding: 0.5rem;
-align-items: center;
-display:flex;
-justify-content:center;
-background-color: ${props => (props.clicked ? 'black' : 'none')};
-color: ${props => (props.clicked ? 'white' : 'black')};
-font-family: 'Source Sans Pro';
-font-style: normal;
-font-weight: 400;
-font-size: 14px;
-`
+  border: 1px solid black;
+  margin: 0 0.25rem;
+  padding: 0.5rem;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  background-color: ${(props) => (props.clicked ? "black" : "none")};
+  color: ${(props) => (props.clicked ? "white" : "black")};
+  font-family: "Source Sans Pro";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+`;
--- src/Components/WithRouter.js
+++ src/Components/WithRouter.js
@@ -1,18 +1,13 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const withRouter = (Component) => {
   const Wrapper = (props) => {
     const navigate = useNavigate();
-    const params =useParams();
-    
-    return (
-      <Component
-        navigate={navigate} 
-        params={params}
-        {...props}
-        />
-    );
+    const params = useParams();
+
+    return <Component navigate={navigate} params={params} {...props} />;
   };
-  
+
   return Wrapper;
 };
