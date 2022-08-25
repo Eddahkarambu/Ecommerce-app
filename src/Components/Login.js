2c0c64391894b429f539ac3bb8f94374425c0eb6
Eddahkarambu <gikundaeddah@gmail.com>
2022-08-25 16:17:53 +0300

    add eventhandlers to the login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,19 +1,46 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailAddress: "",
+      password: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    handleInputChange(event) {
+        event.preventDefault();
+        const target = event.target;
+        this.setState({
+          [target.name]: target.value,
+        });
+      }
+    
+      handleSubmit(event) {
+        event.preventDefault();
+        Userfront.login({
+          method: "password",
+          emailOrUsername: this.state.emailOrUsername,
+          password: this.state.password,
+        });
+      }
+  }
   render() {
     return (
       <div>
-        <div>LOGIN</div>
-        <div>Enter your details here to continue</div>
         <form>
+          <div>LOGIN</div>
+          <div>Enter your details here to continue</div>
           <label>
             Email Address
             <input
               name="emailAddress"
               type="text"
-              value={this.state.emailAddress}
-              onChange={this.handleInputChange}
+                value={this.state.emailAddress}
+                onChange={this.handleInputChange}
             />
           </label>
           <label>
@@ -21,10 +48,13 @@ class Login extends Component {
             <input
               name="password"
               type="password"
-              value={this.state.emailAddress}
-              onChange={this.handleInputChange}
+                value={this.state.password}
+                onChange={this.handleInputChange}
             />
           </label>
+          <div>Forgot password </div>
+          <button>Login</button>
+          <div>Dont have an account? Sign here </div>
         </form>
       </div>
     );
