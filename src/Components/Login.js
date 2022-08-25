77bd1e0aa2b738184c3d613af2abdc98da28d6fb
Eddahkarambu <gikundaeddah@gmail.com>
2022-08-25 15:33:35 +0300

    add input to the login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,1 +1,22 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label>
+            Email Address
+            <input
+              name="Email Address"
+              type="text"
+              //   value={this.state.emailOrUsername}
+              //   onChange={this.handleInputChange}
+            />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+export default Login;
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,11 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import CartPage from "./CartPage";
 import Category from "./Category";
 import Product from "./Product";
-
+import Login from "./Login";
+import Signup from "./Signup";
 export default class Paths extends Component {
   render() {
     return (
       <Routes>
+        <Route path="Login" exact element={<Login />}></Route>
+        <Route path="Signup" exact element={<Signup />}></Route>
         <Route path="/" exact element={<Category />}></Route>
         <Route path="/Product" exact element={<Product />}></Route>
         <Route path="/CartPage" exact element={<CartPage />}></Route>
